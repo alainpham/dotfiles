79397f964da8c51a1c435fc69ff7ba49f4faa684
apham <admin@zez.duckdns.org>
2025-11-23 11:55:15 +0100

    change script

--- root/etc/nixos/configuration.nix
+++ root/etc/nixos/configuration.nix
@@ -21,7 +21,7 @@ let
   dotfilesgit = builtins.fetchGit {
     url = "https://github.com/alainpham/dotfiles.git";
     ref = "master";
-    rev = "38ef9a7dd42c53d9585dc8090a10bd288a8ae5fe";
+    rev = "aa7b05c0d6ecc1482bdec5ed0a3ed9615e448aea";
   };
 
   # desktop related
@@ -100,7 +100,7 @@ let
 
         export APPDIR=$out/bin
 
-        ./webapps/genapps
+        bash -c "./webapps/genapps $WEBAPPSLIST $APPDIR"
       '';
     };
 in
