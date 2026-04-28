110f4c201e742faddadfefb75a47212c9d903b5e
alainpham <13981031+alainpham@users.noreply.github.com>
2026-04-28 15:14:32 +0200

    resample quality

---
+++ home/.config/wireplumber/main.lua.d/99-disable-devices.lua
@@ -0,0 +1,23 @@
+rule = {
+  matches = {
+    {
+      { "device.name", "equals", "alsa_card.platform-snd_dummy.0" },
+    },
+  },
+  apply_properties = {
+    ["device.disabled"] = true,
+  },
+}
+table.insert(alsa_monitor.rules,rule)
+
+rule = {
+  matches = {
+    {
+      { "device.name", "equals", "alsa_card.platform-snd_aloop.0" },
+    },
+  },
+  apply_properties = {
+    ["device.disabled"] = true,
+  },
+}
+table.insert(alsa_monitor.rules,rule)
