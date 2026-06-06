c4ef157f5ba5cc5ff694c850c91c74b854d95694
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-07 01:15:29 +0200

    speedtest

--- osinstall/derivations/speedtest.sh
+++ osinstall/derivations/speedtest.sh
@@ -1,3 +1,5 @@
+
+echo downloading https://install.speedtest.net/app/cli/ookla-speedtest-$SPEEDTEST_VERSION-linux-x86_64.tgz
 curl -L -o /tmp/sptest/speedtest.tgz https://install.speedtest.net/app/cli/ookla-speedtest-$SPEEDTEST_VERSION-linux-x86_64.tgz
 tar -xzf /tmp/sptest/speedtest.tgz -C /tmp/sptest
 cp /tmp/sptest/speedtest /usr/local/bin/speedtest
