948e209b42b430614725b1c8087adf533e2c8e20
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-07 01:37:43 +0200

    speedtest installer

--- osinstall/derivations/speedtest.sh
+++ osinstall/derivations/speedtest.sh
@@ -1,7 +1,15 @@
 
 echo downloading https://install.speedtest.net/app/cli/ookla-speedtest-$SPEEDTEST_VERSION-linux-x86_64.tgz
+mkdir /tmp/sptest
 curl -L -o /tmp/sptest/speedtest.tgz https://install.speedtest.net/app/cli/ookla-speedtest-$SPEEDTEST_VERSION-linux-x86_64.tgz
 tar -xzf /tmp/sptest/speedtest.tgz -C /tmp/sptest
 cp /tmp/sptest/speedtest /usr/local/bin/speedtest
 chmod 755 /usr/local/bin/speedtest
 rm -f /tmp/sptest.tgz
+
+if [ ! -e /etc/ssl/cert.pem ]; then
+  echo creating symlink of pem file
+  ln -s /etc/ssl/ca-bundle.pem /etc/ssl/cert.pem
+else
+  echo "cert.pem exists"
+fi
