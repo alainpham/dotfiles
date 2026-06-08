a625d076e0392fb282dfba023582e6de04c110b1
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-08 23:51:50 +0200

    containers

--- osinstall/tumbleweed/modules/containers.sh
+++ osinstall/tumbleweed/modules/containers.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-zypper in -y docker python-docker docker-compose skopeo skopeo-bash-completion docker-buildx
+zypper in -y docker docker-compose skopeo skopeo-bash-completion docker-buildx
 zypper in -y docker-bash-completion
 
 systemctl enable --now docker
