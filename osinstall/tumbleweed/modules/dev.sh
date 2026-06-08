6b6fd74673c699a20a49a765946d63f36389bce8
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-09 00:17:24 +0200

    code

--- osinstall/tumbleweed/modules/dev.sh
+++ osinstall/tumbleweed/modules/dev.sh
@@ -6,5 +6,15 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 zypper in -y ansible java-25-openjdk-devel maven nodejs-default npm-default go
 zypper in -y apache2 php8 php8-cli apache2-mod_php8 php8-sqlite
 
+
+if [ ! -e /home/$TARGET_USERNAME/.opencode/bin/opencode ]; then 
 curl -fsSL https://opencode.ai/install | sudo -u $TARGET_USERNAME bash
+else
+echo opencode already installed
+fi
+
+if [ ! -e /home/$TARGET_USERNAME/.local/bin/claude ]; then 
 curl -fsSL https://claude.ai/install.sh | sudo -u $TARGET_USERNAME bash
+else
+echo claude already installed
+fi
