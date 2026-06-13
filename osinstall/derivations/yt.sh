e6ec324e2bb6a64aed36d42fe370a281874e7e83
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-13 19:27:46 +0200

    yt

--- osinstall/derivations/yt.sh
+++ osinstall/derivations/yt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+wget -O /usr/local/bin/yt-dlp \
+  https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+
 chmod 755 /usr/local/bin/yt-dlp
