063d482dd5d444a29f531fd5246b898666647a43
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-15 22:11:34 +0200

    gitcore

--- get.sh
+++ get.sh
@@ -5,7 +5,7 @@ if ! command -v git >/dev/null 2>&1; then
     sudo zypper install -y git
   elif command -v apt >/dev/null 2>&1; then
     sudo apt update
-    sudo apt install -y git
+    sudo apt install -y git-core
   else
     echo "Error: git is not installed and no supported package manager was found." >&2
     exit 1
