7276a40b1d84566b2f5762901a9690e9a11d2267
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-16 23:39:39 +0200

    gui flatseal

--- osinstall/tumbleweed/modules/gui.sh
+++ osinstall/tumbleweed/modules/gui.sh
@@ -62,6 +62,7 @@ zypper in -y \
 zypper in -y flatpak
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo -u $TARGET_USERNAME flatpak override --user --filesystem=home
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub org.gnome.Loupe
 flatpak install -y flathub org.kde.haruna
