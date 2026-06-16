a0798e914883c99e15c70ac7deaa095b6be6df79
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-16 14:45:07 +0200

    antimicrox

--- osinstall/tumbleweed/modules/workstation.sh
+++ osinstall/tumbleweed/modules/workstation.sh
@@ -14,17 +14,23 @@ zypper in -y \
     easytag \
     audacity
 
+flatpak install -y flathub fr.handbrake.ghb
+flatpak install -y flathub com.obsproject.Studio
 
-flatpak install flathub fr.handbrake.ghb
-flatpak install flathub org.kde.kdenlive
-flatpak install flathub org.onlyoffice.desktopeditors
-flatpak install flathub com.jgraph.drawio.desktop
-flatpak install flathub org.freac.freac
-flatpak install flathub org.localsend.localsend_app
-flatpak install flathub org.avidemux.Avidemux
-flatpak install flathub com.getpostman.Postman
-flatpak install flathub io.dbeaver.DBeaverCommunity
-flatpak install flathub com.obsproject.Studio
+flatpak install -y flathub io.dbeaver.DBeaverCommunity
+flatpak install -y flathub org.kde.kdenlive
+flatpak install -y flathub org.onlyoffice.desktopeditors
+flatpak install -y flathub com.jgraph.drawio.desktop
+flatpak install -y flathub org.beeref.BeeRef
+flatpak install -y flathub org.freac.freac
+flatpak install -y flathub org.localsend.localsend_app
+flatpak install -y flathub org.avidemux.Avidemux
+flatpak install -y flathub com.getpostman.Postman
+flatpak install -y flathub io.github.antimicrox.antimicrox
+
+flatpak install -y flathub com.viber.Viber
+# flatpak install -y flathub us.zoom.Zoom
+# flatpak install -y flathub com.slack.Slack
 
 # TODO REAPER
 # TODO MLVAPP
--- osinstall/ubuntu/install.sh
+++ osinstall/ubuntu/install.sh
@@ -1217,13 +1217,15 @@ flatpak install -y flathub io.dbeaver.DBeaverCommunity
 flatpak install -y flathub org.kde.kdenlive
 flatpak install -y flathub org.onlyoffice.desktopeditors
 flatpak install -y flathub com.jgraph.drawio.desktop
-flatpak install -y flathub com.viber.Viber
 flatpak install -y flathub org.beeref.BeeRef
 flatpak install -y flathub org.freac.freac
 flatpak install -y flathub org.localsend.localsend_app
 flatpak install -y flathub org.avidemux.Avidemux
 flatpak install -y flathub com.getpostman.Postman
 flatpak install -y flathub io.github.antimicrox.antimicrox
+
+
+flatpak install -y flathub com.viber.Viber
 flatpak install -y flathub us.zoom.Zoom
 flatpak install -y flathub com.slack.Slack
 
