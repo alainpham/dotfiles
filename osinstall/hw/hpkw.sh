f97b49214c4d4df8a2752720e0287ac7d7b97df4
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-17 23:36:33 +0200

    fix intel

--- osinstall/hw/hpkw.sh
+++ osinstall/hw/hpkw.sh
@@ -1,7 +1,7 @@
 
 
 ihw(){
-    rypper in intel-media-driver intel-gpu-tools
+    zypper in intel-media-driver intel-gpu-tools
     zypper in openSUSE-repos-Tumbleweed-NVIDIA
     zypper ref
     zypper in nvidia-driver-G06-kmp-meta
