88b49cc5ed36b6e7e005c437f96ecb7f9df089f2
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-18 18:46:53 +0200

    virt and vmdl

---
+++ osinstall/tumbleweed/modules/virtualization.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+# zypper in -y \
+#     qemu-kvm \
+#     qemu-tools \
+#     libvirt \
+#     libvirt-client \
+#     libvirt-daemon-qemu \
+#     virt-install \
+#     virt-manager \
+#     guestfs-tools \
+#     bridge-utils \
+#     libosinfo \
+#     osinfo-db \
+#     osinfo-db-tools \
+#     mkisofs \
+#     cdrkit-cdrtools-compat
+
+zypper in -y qemu-img libvirt-client libvirt-daemon virt-install guestfs-tools virt-manager bridge-utils libosinfo
