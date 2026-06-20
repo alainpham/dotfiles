0c8332393dd2093512d27583023679af731ff496
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-20 02:21:02 +0200

    apply

--- apply.sh
+++ apply.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 stow --no-folding --target=/home/$USER --adopt home
 git restore .
+
+
+#####################
+echo install all custom scripts
+for dir in "/home/$TARGET_USERNAME/dotfiles/scripts/"*; do
+    if [ -d "$dir" ]; then
+        for file in "$dir"/*; do
+            [ -e "$file" ] || continue
+            cp "$file" /usr/local/bin/
+            echo copied file $file
+        done
+    fi
+done
+
+mkdir -p /usr/local/share/icons
+cp -r /home/$TARGET_USERNAME/dotfiles/icons/* /usr/local/share/icons
+
+
+#####################
+echo copy whole etc folder
+echo network config dnsmasq and powersave
+cp -R /home/$TARGET_USERNAME/dotfiles/etc/* /etc/
