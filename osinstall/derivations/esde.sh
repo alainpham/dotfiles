128d378ac4ec11d69eb0c930bc1c58577413a4de
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-21 18:13:43 +0200

    estation

--- osinstall/derivations/esde.sh
+++ osinstall/derivations/esde.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-export APPNAME=emustation
-export APPIMAGEURL=https://github.com/ilia3101/MLV-App/releases/download/Qtv${MLVAPP_VERSION}/MLVApp.v${MLVAPP_VERSION}.Linux.x86_64.AppImage
+export APPNAME=estation
+export APPIMAGEURL=https://gitlab.com/es-de/emulationstation-de/-/package_files/${ESDE_VERSION_ID}/download
 export APPIMAGEVERSION=${ESDE_VERSION}
 
 bash "/home/$TARGET_USERNAME/dotfiles/osinstall/derivations/common_appimage.sh"
