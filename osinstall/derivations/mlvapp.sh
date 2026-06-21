7e9d565f91fc7be454fb8b36e664a779d8cb3ae2
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-21 17:15:25 +0200

    mlvp app

--- osinstall/derivations/mlvapp.sh
+++ osinstall/derivations/mlvapp.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 export APPNAME=mlvapp
-export APPIMAGEURL=https://github.com/ilia3101/MLV-App/releases/download/QTv${MLVAPP_VERSION}/MLV.App.v${MLVAPP_VERSION}.Linux.x86_64.AppImage
+export APPIMAGEURL=https://github.com/ilia3101/MLV-App/releases/download/Qtv${MLVAPP_VERSION}/MLVApp.v${MLVAPP_VERSION}.Linux.x86_64.AppImage
 export APPIMAGEVERSION=${MLVAPP_VERSION}
 
 bash "/home/$TARGET_USERNAME/dotfiles/osinstall/derivations/common_appimage.sh"
-
