cf9ec9d8439356a8b4a3ead125b1eae71c0684da
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-21 21:33:11 +0200

    ppsspp

--- osinstall/derivations/ppsspp.sh
+++ osinstall/derivations/ppsspp.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 export APPNAME=ppsspp
-export APPIMAGEURL=https://github.com/hrydgard/ppsspp/releases/download/v${PPSSPP_VERSION}/PPSSPP-v${PPSSPP_VERSION}-anylinux-x86_64.AppImag
+export APPIMAGEURL=https://github.com/hrydgard/ppsspp/releases/download/v${PPSSPP_VERSION}/PPSSPP-v${PPSSPP_VERSION}-anylinux-x86_64.AppImage
 export APPIMAGEVERSION=${PPSSPP_VERSION}
 
 bash "/home/$TARGET_USERNAME/dotfiles/osinstall/derivations/common_appimage.sh"
