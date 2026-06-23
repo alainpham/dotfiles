8208a37258a85fb2601d76b63c4d319bd8babc05
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-24 00:38:01 +0200

    lpro hw

--- osinstall/hw/lpro.sh
+++ osinstall/hw/lpro.sh
@@ -1,5 +1,8 @@
 
 
 ihw(){
-    zypper in -y radeontop
+    fprintd-enroll
+    echo "auth required pam_fprintd.so"| sudo tee /etc/pam.d/sudo
+    echo "auth required pam_fprintd.so"| sudo tee /etc/pam.d/login
+    echo "auth required pam_fprintd.so"| sudo tee /etc/pam.d/lightdm
 }
