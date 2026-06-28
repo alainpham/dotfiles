74627b724a05f513a66ca4252d880aeb2818bbab
alainpham <13981031+alainpham@users.noreply.github.com>
2026-06-28 21:44:41 +0200

    gaming

--- osinstall/tumbleweed/modules/gaming.sh
+++ osinstall/tumbleweed/modules/gaming.sh
@@ -19,4 +19,6 @@ zypper in -y dolphin-emu
 
 bash /home/$TARGET_USERNAME/dotfiles/osinstall/derivations/cemu.sh
 
+bash /home/$TARGET_USERNAME/dotfiles/osinstall/derivations/mgba.sh
+
 bash /home/$TARGET_USERNAME/dotfiles/osinstall/derivations/gshorts.sh
