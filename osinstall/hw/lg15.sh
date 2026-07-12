f2ed6cf0bb45f2eb81e19f9743fdc1e2a269fc00
alainpham <13981031+alainpham@users.noreply.github.com>
2026-07-12 21:14:53 +0200

    lg15

--- osinstall/hw/lg15.sh
+++ osinstall/hw/lg15.sh
@@ -4,4 +4,6 @@ ihw(){
     zypper in -y --auto-agree-with-licenses openSUSE-repos-Tumbleweed-NVIDIA
     zypper ref
     zypper in -y --auto-agree-with-licenses nvidia-driver-G06-kmp-meta
+    # SOF firmware for Intel Alder Lake DSP audio (internal speakers/mics)
+    zypper in -y --auto-agree-with-licenses sof-firmware
 }
