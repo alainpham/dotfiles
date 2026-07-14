9252dfb6148464fe970c51cb8a9d776f5be40158
alainpham <13981031+alainpham@users.noreply.github.com>
2026-07-15 01:45:20 +0200

    dev

--- osinstall/ubuntu/modules/dev.sh
+++ osinstall/ubuntu/modules/dev.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 apt install -y ansible openjdk-25-jdk maven nodejs npm golang-go
 apt install -y apache2 php php-cli libapache2-mod-php php-sqlite3
 
+systemctl disable apache2.service 
