a046c331a1b7707f8442523385be2bdd18a374b1
alainpham <13981031+alainpham@users.noreply.github.com>
2026-07-14 16:00:35 +0200

    install cuda

--- osinstall/hw/lg15.sh
+++ osinstall/hw/lg15.sh
@@ -7,4 +7,10 @@ ihw(){
     zypper in -y --auto-agree-with-licenses nvidia-driver-G06-kmp-meta
     # SOF firmware for Intel Alder Lake DSP audio (internal speakers/mics)
     zypper in -y --auto-agree-with-licenses sof-firmware
+
+    mkdir /tmp/cuda
+    cd /tmp/cuda
+    wget https://developer.download.nvidia.com/compute/cuda/13.3.1/local_installers/cuda_13.3.1_610.43.02_linux.run
+    sudo sh cuda_13.3.1_610.43.02_linux.run
+    echo "/usr/local/cuda/lib64" | tee /etc/ld.so.conf.d/cuda.conf
 }
