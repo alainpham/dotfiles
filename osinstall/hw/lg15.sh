aca5890e20ad97e44cc6fd2f3d8dc5f36f322d61
alainpham <13981031+alainpham@users.noreply.github.com>
2026-08-07 01:46:38 +0200

    nvidia update

--- osinstall/hw/lg15.sh
+++ osinstall/hw/lg15.sh
@@ -4,7 +4,7 @@ ihw(){
     zypper in -y --auto-agree-with-licenses intel-media-driver intel-gpu-tools
     zypper in -y --auto-agree-with-licenses openSUSE-repos-Tumbleweed-NVIDIA
     zypper ref
-    zypper in -y --auto-agree-with-licenses nvidia-driver-G06-kmp-meta
+    zypper in -y --auto-agree-with-licenses nvidia-open-driver-G07-signed-kmp-meta
     # SOF firmware for Intel Alder Lake DSP audio (internal speakers/mics)
     zypper in -y --auto-agree-with-licenses sof-firmware
 
